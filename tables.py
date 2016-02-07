3701b3869c02b187f7c36c76dab700c3b8cdaee7
Alex Immer <alexander.immer@yahoo.com>
2016-02-07 15:28:03 +0100

    Add sensor recording columns to table

--- tables.py
+++ tables.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy import PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -25,8 +25,7 @@ class SensorSession(Base):
 
     # SQL definitions
     id = Column(Integer, primary_key=True)
-    created = Column(DateTime, default=datetime.datetime.now())
-    # or Column(DateTime, default=datetime.datetime.now) - defers time a bit
+    created = Column(DateTime, default=datetime.datetime.now)
     user_id = Column(Integer, ForeignKey('user.id'))
 
     # ORM definitions for simple access
@@ -38,9 +37,40 @@ class SensorData(Base):
     __tablename__ = 'sensor_data'
 
     # SQL definitions
+    # Keys
     session_id = Column(Integer, ForeignKey('sensor_session.id'))
     nano_time = Column(Integer)
     PrimaryKeyConstraint(session_id, nano_time)
+    # Sensor recordings
+    acc_x     = Column(Float, nullable=True)
+    acc_y     = Column(Float, nullable=True)
+    acc_z     = Column(Float, nullable=True)
+    lin_acc_x = Column(Float, nullable=True)
+    lin_acc_y = Column(Float, nullable=True)
+    lin_acc_z = Column(Float, nullable=True)
+    gyr_x     = Column(Float, nullable=True)
+    gyr_y     = Column(Float, nullable=True)
+    gyr_z     = Column(Float, nullable=True)
+    rot_x     = Column(Float, nullable=True)
+    rot_y     = Column(Float, nullable=True)
+    rot_z     = Column(Float, nullable=True)
+    mag_x     = Column(Float, nullable=True)
+    mag_y     = Column(Float, nullable=True)
+    mag_z     = Column(Float, nullable=True)
+    lat       = Column(Float, nullable=True)
+    lon       = Column(Float, nullable=True)
+    bearing   = Column(Float, nullable=True)
+    speed     = Column(Float, nullable=True)
+    alt       = Column(Float, nullable=True)
+    err_lat   = Column(Float, nullable=True)
+    err_lon   = Column(Float, nullable=True)
+    pressure  = Column(Float, nullable=True)
+    station   = Column(Float, nullable=True)
+    run       = Column(Float, nullable=True)
+    walk      = Column(Float, nullable=True)
+    auto      = Column(Float, nullable=True)
+    cycling   = Column(Float, nullable=True)
+    unknown   = Column(Float, nullable=True)
 
     # ORM definitions for simple access
     session = relationship("sensor_session", back_populates='sensor_data')
