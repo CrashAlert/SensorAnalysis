f4451305aac31f461a4968bf1b0183ae86a2f09a
Alex Immer <alexander.immer@yahoo.com>
2016-02-08 19:54:48 +0100

    Add tests for user

--- testTables.py
+++ testTables.py
@@ -1,10 +1,30 @@
 import unittest
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tables import Base, User, SensorSession, SensorData
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestDB(unittest.TestCase):
+    def setUp(self):
+        self.engine = create_engine('sqlite:///:memory:')
+        Base.metadata.create_all(self.engine)
+        self.DBsession = sessionmaker(self.engine)
+
+    def test_user(self):
+        # setup user and relationship
+        user = User(name='username')
+        sess = SensorSession()
+        sess.user = user
+
+        # commit to database
+        session = self.DBsession()
+        session.add_all([user, sess])
+        session.commit()
+
+        assert user.name == 'username'
+        assert user.id is not None
+        assert sess.id is not None
+        assert sess.created is not None
+        assert sess.user_id == user.id
 
 
-if __name__ == '__main__':
-    unittest.main()
