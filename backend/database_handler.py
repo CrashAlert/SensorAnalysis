1b9fbf694edb79226f6a04c84f01411f6ad753fb
Alex Immer <alexander.immer@yahoo.com>
2016-02-22 01:43:12 +0100

    Add todos

--- backend/database_handler.py
+++ backend/database_handler.py
@@ -1,7 +1,11 @@
 from backend.tables import User, SensorSession, SensorData
 
 def store_session(name, data):
+    # find respective user
+    # add session and for each row in csv add sensor data
     return
 
 def get_session_as_json(name):
+    # find session by name
+    # get each related sensor data row
     return
